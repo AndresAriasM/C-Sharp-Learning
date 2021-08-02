9b56803dbf279fe7558f3022da67dcd4e5301afb
Andrés Arias Medina <77759820+AndresAriasM@users.noreply.github.com>
2021-08-02 17:20:04 -0500

    Create #deCaracteresEnString.cs

---
+++ Basics/#deCaracteresEnString.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace #deCaracteresEnString
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Ingrese una palabra aleatoria");
+            string Entrada = Console.ReadLine();
+
+            Console.WriteLine("Ahora ingrese una letra perteneciente a la palabra que anteriormente ingresaste, el programa te dira cuantas veces esta esa letra en la palabra");
+            string caracter = Console.ReadLine();
+
+            int contador = Regex.Matches(Entrada,caracter).Count; // Expresión ligada a (using System.Text.RegularExpressions;)
+
+            Console.WriteLine("La letra seleccionada se repite " + contador + " veces");
+            Console.WriteLine("Fin del programa");
+
+            Console.Read();
+        }
+    }
+
+}
